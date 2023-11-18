27f7672a4a87e9f0a6f57c3193a214dfad02c0ee
Shahid Khan <shahidseran786@gmail.com>
2023-11-18 20:38:29 +0530

    Backtracking LC ques

--- Backtacking/LC(22)GenrateParenthesis.cpp
+++ Backtacking/LC(22)GenrateParenthesis.cpp
@@ -1,3 +1,17 @@
+/*Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+*/
+
 class Solution {
 public:
     void solve(vector<string> &ans,int n,int open,int close,string output){
